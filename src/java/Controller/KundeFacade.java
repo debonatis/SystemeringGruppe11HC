9a81ec428bbf4e1de7838892a51d0530222af673
deb <deb@deb-BN.ka.hist.no>
2013-03-13 21:17:25 +0100

    Han helvett

--- src/java/Controller/KundeFacade.java
+++ src/java/Controller/KundeFacade.java
@@ -32,24 +32,4 @@ public class KundeFacade extends AbstractFacade<Kunde> {
     public KundeFacade() {
         super(Kunde.class);
     }
-
-    @GET
-    @Produces("text/html")
-    public String getGoogleMap() {
-// Drag and drop the getGoogleMap operation here
-
-        try {
-            String address = "16 Network Circle, Menlo Park";
-            java.lang.Integer zoom = 15;
-            String iframe = "false";
-
-            RestResponse result = GoogleMapService.getGoogleMap(address, zoom, iframe);
-            //TODO - Uncomment the print Statement below to print result.
-           return result.getDataAsString();
-        } catch (Exception ex) {
-            ex.printStackTrace();
-        }
-
-        return "";
-    }
 }
