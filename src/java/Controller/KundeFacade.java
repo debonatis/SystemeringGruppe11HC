ca981373e46bb7018d6bbb8f18e68531429e4d88
deb <deb@deb-BN.ka.hist.no>
2013-03-13 21:12:22 +0100

    Han helvett

--- src/java/Controller/KundeFacade.java
+++ src/java/Controller/KundeFacade.java
@@ -6,8 +6,13 @@ package Controller;
 
 import Modell.Kunde;
 import javax.ejb.Stateless;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.GET;
+import javax.ws.rs.Produces;
+import org.netbeans.saas.google.GoogleMapService;
+import org.netbeans.saas.RestResponse;
 
 /**
  *
@@ -15,6 +20,7 @@ import javax.persistence.PersistenceContext;
  */
 @Stateless
 public class KundeFacade extends AbstractFacade<Kunde> {
+
     @PersistenceContext(unitName = "HealthyCateringPU")
     private EntityManager em;
 
@@ -26,5 +32,24 @@ public class KundeFacade extends AbstractFacade<Kunde> {
     public KundeFacade() {
         super(Kunde.class);
     }
-    
+
+    @GET
+    @Produces("text/html")
+    public String getGoogleMap() {
+// Drag and drop the getGoogleMap operation here
+
+        try {
+            String address = "16 Network Circle, Menlo Park";
+            java.lang.Integer zoom = 15;
+            String iframe = "false";
+
+            RestResponse result = GoogleMapService.getGoogleMap(address, zoom, iframe);
+            //TODO - Uncomment the print Statement below to print result.
+           return result.getDataAsString();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return "";
+    }
 }
